59a80cd33d814240fb77da2e06de5d0a1b37118b
iRuMa <40019019+iRuMa17@users.noreply.github.com>
2019-11-27 12:21:08 +0900

    Update main.py
    0.2v

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ import statistics
 from urllib.request import urlopen
 from datetime import date
 import zlib
+import numpy as np
+import speech_recognition as sr
+
+r = sr.Recognizer()
+mic = sr.Microphone()
