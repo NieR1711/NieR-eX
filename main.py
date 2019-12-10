434197ce0d6f2c810dbde9c7fd4cc2f6356354dc
NieR <40019019+NieR1711@users.noreply.github.com>
2019-12-10 10:49:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,29 @@ import sys
 import re
 import math
 import random
+import date
+import time
 import statistics
 from urllib.request import urlopen
 from datetime import date
 import zlib
 import numpy as np
 import speech_recognition as sr
-
+import shutil
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
 r = sr.Recognizer()
 mic = sr.Microphone()
 
 with mic as source:
   audio = r.listen(source)
 print(r.recognize_google(audio.language='ko-KR'))
+
+x = np.arange(0,6,0.1)
+y = np.sin(x)
+
+plt.plot(x,y)
+plt.show()
+
 
